271a42a0f3017e9f1afdf8d4b4406eb33ee211e5
Helena <hexylena@galaxians.org>
2023-09-04 10:23:48 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -53,7 +53,6 @@ self.addEventListener('activate', event => {
 // from the network before returning it to the page.
 self.addEventListener('fetch', event => {
   // Skip cross-origin requests, like those for Google Analytics.
-  console.log(event.request.url);
   if (true || event.request.url.startsWith(self.location.origin)) {
     event.respondWith(
       caches.match(event.request).then(cachedResponse => {
